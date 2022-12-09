dfd7c6fe7478d4ff90e35a020b84c44f87695e24
Alihan KUZUCUK <95879997+alihankuzucuk@users.noreply.github.com>
2022-12-09 13:56:11 +0300

    BaseViewController with MaterialActivityIndicator and GameListViewController has been created

---
+++ GameBox/GameBox/Core/BaseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseViewController.swift
+//  GameBox
+//
+//  Created by Alihan KUZUCUK on 9.12.2022.
+//
+
+import UIKit
+import MaterialActivityIndicator
+
+class BaseViewController: UIViewController {
+    
+    let indicator = MaterialActivityIndicatorView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupActivityIndicatorView()
+    }
+    
+    private func setupActivityIndicatorView() {
+        view.addSubview(indicator)
+        setupActivityIndicatorViewConstraints()
+    }
+    
+    private func setupActivityIndicatorViewConstraints() {
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        indicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+}
---
+++ GameBox/GameBox/Scenes/GameList/Controller/GameListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameListViewController.swift
+//  GameBox
+//
+//  Created by Alihan KUZUCUK on 9.12.2022.
+//
+
+import UIKit
+
+final class GameListViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
